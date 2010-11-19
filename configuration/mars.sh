fcc902f70aa5d323995043e98a3d39d234be742d
Thomas Roehr <thomas.roehr@dfki.de>
2010-11-19 15:42:24 +0100

    Add mars core content

---
+++ configuration/mars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#set console language to english (to properly load .scn files
+export LANG="en_EN.UTF-8"
+export LANGUAGE="en_EN.UTF-8"
+export LC_ALL="en_EN.UTF-8"
+export LC_ALL="C"
+#start the simulator using the params given to this script
+../build/simulation
---
+++ tasks/Mars.cpp
@@ -0,0 +1,86 @@
+#include "Mars.hpp"
+
+#include <rtt/NonPeriodicActivity.hpp>
+
+
+using namespace simulation;
+
+
+RTT::NonPeriodicActivity* Mars::getNonPeriodicActivity()
+{ return dynamic_cast< RTT::NonPeriodicActivity* >(getActivity().get()); }
+
+
+Mars::Mars(std::string const& name)
+    : MarsBase(name), initDone(false)
+{
+    int ret = pthread_create(&thread_info, NULL, startMarsFunc, this); 
+    if(ret)
+	throw std::runtime_error("Failed to create MARS thread");
+
+}
+
+
+void* Mars::startMarsFunc(void* a)
+{
+    int argc = 0;
+    char **argv = 0; 
+    
+    Mars *mars = static_cast<simulation::Mars *>(a);
+    mars->simulatorInterface = SimulatorInterface::getInstance();
+    
+    mars->initDone = true;
+    mars->simulatorInterface->runSimulation(argc, argv);
+    
+    return 0;
+}
+
+
+
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See Mars.hpp for more detailed
+// documentation about them.
+
+bool Mars::configureHook()
+{
+
+     while(!initDone)
+  	usleep(10000);
+/*
+     ControlCenter* controlCenter = simulatorInterface->getControlCenter();
+     PluginInterface* plugin = new RimresEnv(controlCenter);
+
+     pluginStruct pstruct;
+     pstruct.name = "RimresPlugin";
+     pstruct.p_interface = plugin;
+     pstruct.p_destroy = NULL;
+
+     
+     simulatorInterface->addPlugin(pstruct);
+*/
+     return true;
+
+}
+
+
+bool Mars::startHook()
+{
+    return true;
+}
+
+void Mars::updateHook()
+{
+}
+
+void Mars::errorHook()
+{
+}
+
+void Mars::stopHook()
+{
+}
+
+void Mars::cleanupHook()
+{
+}
+

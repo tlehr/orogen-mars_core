9fcebbfe6e6782fbc83e44032c84a47feb5fbae4
Janosch Machowinski <janosch.machowinski@dfki.de>
2013-02-28 18:40:46 +0100

    added MarsActuator

---
+++ tasks/MarsActuator.cpp
@@ -0,0 +1,207 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "MarsActuator.hpp"
+#include <mars/interfaces/sim/SimulatorInterface.h>
+//#include <mars_sim/ControlCenter.h>
+#include "MarsPlugin.hpp"
+#include <mars/sim/SimMotor.h>
+#include <mars/interfaces/sim/MotorManagerInterface.h>
+#include <mars/interfaces/sim/NodeManagerInterface.h>
+
+using namespace simulation;
+
+MarsActuator::MarsActuator(std::string const& name)
+    : MarsActuatorBase(name)
+{
+}
+
+MarsActuator::MarsActuator(std::string const& name, RTT::ExecutionEngine* engine)
+    : MarsActuatorBase(name, engine)
+{
+}
+
+MarsActuator::~MarsActuator()
+{
+}
+
+namespace simulation {
+struct ActuatorPlugin : public MarsPlugin
+{
+    MarsActuator *task;
+    //actuator id in the task.
+    int actuatorId;
+    int marsMotorId;
+    base::actuators::DRIVE_MODE mode;
+    double value;
+    bool gotCommand;
+    
+    ActuatorPlugin(const std::string& name, MarsActuator *task, int actuatorId)
+        : task(task), actuatorId(actuatorId), marsMotorId(0), gotCommand(false)
+    {
+        marsMotorId = control->motors->getID( name );
+        if( !marsMotorId )
+            throw std::runtime_error("There is no motor by the name of " + name + " in the scene");	
+    }
+
+    void setCommand(base::actuators::DRIVE_MODE mode, double value)
+    {
+	this->mode = mode;
+	this->value = value;
+	gotCommand = true;
+    }
+    
+    void update( double time )
+    {
+        mars::sim::SimMotor *motor = control->motors->getSimMotor(marsMotorId);
+
+	if(gotCommand)
+	{
+	    switch(mode)
+	    {
+		case base::actuators::DM_PWM:
+		    throw std::runtime_error("No clue how to implementent PWM in mars");
+		    break;
+		case base::actuators::DM_SPEED:
+		    motor->setVelocity(value);
+		    break;
+		case base::actuators::DM_POSITION:
+		    motor->setValue( value );
+		    break;
+		default:
+		    throw std::runtime_error("Given controller mode is not mapped to firmware controller");
+		    break;
+	    }
+	}
+	
+	base::actuators::MotorState ms;
+	ms.current =  motor->getTorque();
+	ms.position = motor->getValue();
+	ms.positionExtern = 0;
+	ms.pwm = 0;
+	task->setNewActuatorStatus(actuatorId, base::Time::fromSeconds(time), ms);	    
+    }
+};
+}
+
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See MarsActuator.hpp for more detailed
+// documentation about them.
+
+void MarsActuator::setCommand(int32_t actuatorId, base::actuators::DRIVE_MODE mode, double value)
+{
+    ActuatorPlugin *plugin = plugins[actuatorId];
+    
+    plugin->setCommand(mode, value);
+}
+
+
+void MarsActuator::statusDispatchAdded(int dispatchId, std::vector< int > actuatorIds)
+{
+
+}
+
+
+bool MarsActuator::configureHook()
+{
+    
+    if (! ::interfaces::Actuator::configureHook())
+        return false;
+        
+    return true;
+    
+}
+
+
+
+bool MarsActuator::startHook()
+{
+    std::cout << "ACTUATOR START HOOK" << std::endl;
+    if (! ::interfaces::Actuator::startHook())
+        return false;
+    
+
+    std::vector<std::string> motorNames = _names.get();    
+    
+    for(std::vector<OrocosCommandDispatcher *>::const_iterator it = cmdDispatches.begin();
+	it != cmdDispatches.end();it++)
+    {
+	const std::vector<int32_t> ids = (*it)->getActuatorIds();
+	for(std::vector<int32_t>::const_iterator id = ids.begin(); id != ids.end(); id++)
+	{
+	    if(motorNames.size() < *id)
+	    {
+		std::cout << "Can not dispatch nonexisting actuator " << *id << std::endl;
+		return false;
+	    }
+
+	    ActuatorPlugin *plugin;
+	    try {
+		plugin = new ActuatorPlugin(motorNames[*id], this, *id);
+	    } catch (std::runtime_error e)
+	    {
+		std::cout << e.what() << std::endl;
+		//TODO CLEANUP MEMORY
+		return false;
+	    }
+	    
+	    plugins.push_back(plugin);
+	}
+    }
+    std::cout << "ACTUATOR START HOOK OK" << std::endl;
+    
+    return true;
+    
+}
+
+
+
+void MarsActuator::updateHook()
+{
+    ::interfaces::Actuator::updateHook();
+    
+
+    processDispatched();
+
+    
+}
+
+
+
+void MarsActuator::errorHook()
+{
+    
+    ::interfaces::Actuator::errorHook();
+    
+
+    
+
+    
+}
+
+
+
+void MarsActuator::stopHook()
+{
+    
+    ::interfaces::Actuator::stopHook();
+    
+
+    
+
+    
+}
+
+
+
+void MarsActuator::cleanupHook()
+{
+    
+    ::interfaces::Actuator::cleanupHook();
+    
+
+    
+
+    
+}
+

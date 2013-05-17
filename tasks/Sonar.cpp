220f03f05607a1d9290cce0e237bcb060ec2e693
Tilo Eichhorn <tilodsi@tzi.de>
2013-05-17 11:18:45 +0200

    development of Sonar Task

---
+++ tasks/Sonar.cpp
@@ -0,0 +1,176 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+/*
+
+task_context "SonarTop" do
+        output_port("sonar_beam","base::samples::SonarBeam").
+            doc('top sonar beam')
+
+        property("left_limit","double",Math::PI)
+        property("right_limit","double",-Math::PI)
+        property("resolution","double",0.1)
+        property("maximum_distance","double",100.0)
+        property("ping_pong_mode","bool",false).
+            doc('if true ping pong mode is activated')
+end
+
+task_context "SonarRear" do
+        output_port("sonar_beam","base::samples::SonarBeam").
+            doc('rear sonar beam')
+
+        property("left_limit","double",0.7*Math::PI)
+        property("right_limit","double",0.3*Math::PI)
+        property("resolution","double",0.1)
+        property("maximum_distance","double",50.0)
+        property("ping_pong_mode","bool",true).
+            doc('if true ping pong mode is activated')
+end
+*/
+#include "Sonar.hpp"
+#include <mars/sim/ScanningSonar.h>
+#include <mars/interfaces/sim/NodeManagerInterface.h>
+#include <mars/interfaces/sim/SensorManagerInterface.h>
+#include <mars/interfaces/sim/ControlCenter.h>
+
+using namespace simulation;
+
+
+Sonar::Sonar(std::string const& name)
+    : SonarBase(name)
+{
+}
+
+Sonar::Sonar(std::string const& name, RTT::ExecutionEngine* engine)
+    : SonarBase(name, engine)
+{
+}
+
+Sonar::~Sonar()
+{
+	//pthread_mutex_destroy(sonar_update_mutex);
+}
+
+bool Sonar::startHook()
+{
+    if (!SonarBase::startHook())
+        return false;
+	//pthread_mutex_init(sonar_update_mutex, NULL);
+	node_id = control->nodes->getID(_node_name.get());
+	std::cout << _node_name.get();
+    return true;
+}
+
+void Sonar::updateHook()
+{
+    SonarBase::updateHook();
+
+    simulation::SonarConfig config;
+    config.start_angle = _left_limit.get();
+    config.end_angle = _right_limit.get();
+    config.max_distance = _maximum_distance.get();
+    config.distance_resolution = _resolution.get();
+    config.ping_pong_mode = _ping_pong_mode.get();
+
+
+    //pthread_mutex_lock(sonar_update_mutex);
+    sonar_config = config;
+    mars::sim::ScanningSonar* sonar = dynamic_cast<mars::sim::ScanningSonar*>(control->sensors->getSimSensor(node_id));
+    sonar->setPingPongConfig(config.ping_pong_mode, config.start_angle, config.end_angle);
+    //pthread_mutex_unlock(sonar_update_mutex);
+
+    if(getSonarData(sonar_beam)){
+       if (_sonar_beam.connected())
+           _sonar_beam.write(sonar_beam);
+    }
+    else{
+        sonar_beam.beam.clear();
+    }
+
+}
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See Sonar.hpp for more detailed
+// documentation about them.
+
+// bool Sonar::configureHook()
+// {
+//     if (! SonarBase::configureHook())
+//         return false;
+//     return true;
+// }
+
+// void Sonar::updateHook()
+// {
+//     SonarBase::updateHook();
+// }
+// void Sonar::errorHook()
+// {
+//     SonarBase::errorHook();
+// }
+// void Sonar::stopHook()
+// {
+//     SonarBase::stopHook();
+// }
+// void Sonar::cleanupHook()
+// {
+//     SonarBase::cleanupHook();
+// }
+
+bool Sonar::getSonarData(base::samples::SonarBeam &sonar_beam){
+    //just return false if the simulation is not running
+    if(!control->sim->isSimRunning())
+      return false;
+
+    mars::interfaces::sReal *sensor_data=0;
+
+    //get data
+    //pthread_mutex_lock(sonar_update_mutex);
+    int n = control->sensors->getSensorData(node_id, &sensor_data);
+    //pthread_mutex_unlock(sonar_update_mutex);
+
+    if(sensor_data == NULL){
+        printf("No Sensor data!\n");
+        return false;
+    }
+
+    sonar_beam.time = base::Time::now();
+    sonar_beam.speed_of_sound = sonar_config.speed_of_sound;
+    sonar_beam.beamwidth_horizontal= sonar_config.beamwidth_horizontal;
+    sonar_beam.beamwidth_vertical= sonar_config.beamwidth_vertical;
+    sonar_beam.sampling_interval = sonar_config.distance_resolution * 2.0 /sonar_config.speed_of_sound;
+
+
+    if(n == 2){ //Asume Ray Sensor
+        sonar_beam.bearing = base::Angle::fromRad(sensor_data[0]);
+        int number_of_values = sonar_config.max_distance/sonar_config.distance_resolution+1;
+        int index = sensor_data[1]/sonar_config.distance_resolution+0.49999;
+        sonar_beam.beam.resize(number_of_values);
+        memset(&sonar_beam.beam[0], sonar_config.default_value, number_of_values);
+
+        if(index < number_of_values)
+        {
+            for(int i = std::max(index-2,0);i<std::min(index+3,number_of_values);i++)
+                sonar_beam.beam.at(i) = sonar_config.default_response_value*5.0/(rand()%5+1);
+        }
+    }else if(n > 2){
+
+        int number_of_values = n-1;
+        sonar_beam.beam.resize(number_of_values);
+        sonar_beam.bearing = base::Angle::fromRad(sensor_data[0]);
+        for(int i=0;i<number_of_values;i++){
+            sonar_beam.beam[i] = sensor_data[i+1];
+        }
+
+    }else {
+        sonar_beam.beam.clear();
+        std::cout << "no sonar data" << std::endl;
+        return false;
+    }
+
+    if(sensor_data)
+    delete[] sensor_data;
+    return true;
+}
+
+void Sonar::update( double time ) {
+}
---
+++ tasks/Sonar.hpp
@@ -0,0 +1,134 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.hpp */
+
+#ifndef SIMULATION_SONAR_TASK_HPP
+#define SIMULATION_SONAR_TASK_HPP
+
+#include "simulation/SonarBase.hpp"
+
+namespace simulation {
+
+namespace simulation
+{
+    //sonar configuration should be implemented in mars
+    struct SonarConfig
+    {
+        SonarConfig()
+        {
+            motor_speed = 1;
+            speed_of_sound = 1500;
+            distance_resolution = 0.1;
+            min_distance = 1.0;
+            max_distance = 50;
+            beamwidth_vertical = 35.0/180*M_PI;
+            beamwidth_horizontal = 6.0/180*M_PI;
+            start_angle = M_PI;
+            end_angle = -M_PI;
+
+            default_response_value = 255;
+            default_value = 0;
+
+            ping_pong_mode = false;
+        };
+
+        float motor_speed;
+        float speed_of_sound;
+        float distance_resolution;
+        float min_distance;
+        float max_distance;
+        float beamwidth_vertical;
+        float beamwidth_horizontal;
+
+        float start_angle;
+        float end_angle;
+
+        bool ping_pong_mode;
+
+        unsigned char default_response_value;
+        unsigned char default_value;
+    };
+}
+
+    class Sonar : public SonarBase
+    {
+	friend class SonarBase;
+    protected:
+		base::samples::SonarBeam sonar_beam;
+		unsigned long node_id;
+        simulation::SonarConfig sonar_config;
+        int sonar_motor_direction;
+        //pthread_mutex_t* sonar_update_mutex;
+
+        void update( double time );
+
+
+    public:
+        Sonar(std::string const& name = "simulation::Sonar");
+        Sonar(std::string const& name, RTT::ExecutionEngine* engine);
+
+        double getSonarBeamBearing(unsigned long sonar_mounting_id);
+        void updateTopSonarPosition();
+
+        bool getSonarData(base::samples::SonarBeam &sonar_beam);
+	~Sonar();
+
+        /** This hook is called by Orocos when the state machine transitions
+         * from PreOperational to Stopped. If it returns false, then the
+         * component will stay in PreOperational. Otherwise, it goes into
+         * Stopped.
+         *
+         * It is meaningful only if the #needs_configuration has been specified
+         * in the task context definition with (for example):
+         *
+         *   task_context "TaskName" do
+         *     needs_configuration
+         *     ...
+         *   end
+         */
+        // bool configureHook();
+
+        /** This hook is called by Orocos when the state machine transitions
+         * from Stopped to Running. If it returns false, then the component will
+         * stay in Stopped. Otherwise, it goes into Running and updateHook()
+         * will be called.
+         */
+         bool startHook();
+
+        /** This hook is called by Orocos when the component is in the Running
+         * state, at each activity step. Here, the activity gives the "ticks"
+         * when the hook should be called.
+         *
+         * The error(), exception() and fatal() calls, when called in this hook,
+         * allow to get into the associated RunTimeError, Exception and
+         * FatalError states.
+         *
+         * In the first case, updateHook() is still called, and recover() allows
+         * you to go back into the Running state.  In the second case, the
+         * errorHook() will be called instead of updateHook(). In Exception, the
+         * component is stopped and recover() needs to be called before starting
+         * it again. Finally, FatalError cannot be recovered.
+         */
+         void updateHook();
+
+        /** This hook is called by Orocos when the component is in the
+         * RunTimeError state, at each activity step. See the discussion in
+         * updateHook() about triggering options.
+         *
+         * Call recover() to go back in the Runtime state.
+         */
+        // void errorHook();
+
+        /** This hook is called by Orocos when the state machine transitions
+         * from Running to Stopped after stop() has been called.
+         */
+        // void stopHook();
+
+        /** This hook is called by Orocos when the state machine transitions
+         * from Stopped to PreOperational, requiring the call to configureHook()
+         * before calling start() again.
+         */
+        // void cleanupHook();
+    };
+}
+
+#endif
+

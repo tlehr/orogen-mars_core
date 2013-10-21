c2fa62f2cf6b223b0416f1ce4595796b683b223a
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2013-10-21 11:45:41 +0200

    added tiemstamp to _calibrated_sensors

--- tasks/MarsIMU.cpp
+++ tasks/MarsIMU.cpp
@@ -78,11 +78,13 @@ void MarsIMU::update( double time )
 
     _orientation_samples.write( rbs );
     
+    imusens.time = getTime();
     imusens.acc = control->nodes->getLinearAcceleration( node_id );
     imusens.gyro = control->nodes->getAngularVelocity( node_id);
     _calibrated_sensors.write( imusens );
 
     rbs.position = control->nodes->getPosition( node_id );
+
     _pose_samples.write( rbs );
 }
 

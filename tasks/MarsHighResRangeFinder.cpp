63eb031f274779036c7a4fffb41d9168c4f8325e
Janosch Machowinski <janosch.machowinski@dfki.de>
2013-11-09 11:38:24 +0100

    created a second non working implementation of the velodyne ;-)

--- tasks/MarsHighResRangeFinder.cpp
+++ tasks/MarsHighResRangeFinder.cpp
@@ -1,5 +1,5 @@
 /* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
-
+#include <math.h>
 #include "MarsHighResRangeFinder.hpp"
 #include <mars/interfaces/sim/SensorManagerInterface.h>
 
@@ -183,119 +183,74 @@ void MarsHighResRangeFinder::getData()
     multi_level_scan.min_range = _minimum_distance.get() * 1000;
     multi_level_scan.max_range = _maximum_distance.get() * 1000;
     
-    float distance = 0;
-    for(it = mCameras.begin(); it < mCameras.end(); ++it) {
-        counter = 0;
-        // Request image and store it within the base distance image.
-        (*it)->camera_sensor->getDepthImage((*it)->image->data);
-          
-        // Run through the image horizontally.  
-        for(double x = (*it)->left_pixel; x < (*it)->right_pixel; x += (*it)->h_steps_pixel) {
-        
-            velodyne_lidar::MultilevelLaserScan::VerticalMultilevelScan vertical_multi_level_scan;
-            // Calculate the current horizontal angle, camera with orientation 0 
-            // would be (-45,45], orientation 90 (45,135] ...
-            //vertical_multi_level_scan.horizontal_angle.rad = 
-            //        (x - (*it)->width/2.0) / (*it)->pixel_per_rad_horizontal - (*it)->rotation_y;
-            // TODO: Do the same angle calculation like you did for the horizontal angle.
+    it = mCameras.begin();
+    double verticalOpeningAngle = 30.0 / 180.0 * M_PI;
+    double verticalStartAngle = -15.0 / 180.0 * M_PI;
+    double scansVertical = 30;
+    double scansHorizontal = 1000;
 
+    double stepHorizontal = M_PI *2 / scansHorizontal;
+    double stepVertical = verticalOpeningAngle / scansVertical;
+    
+    
+    double curAngle = 0;
+    
+    float distance = 0;
         
-            // Run through the image vertically.
-            bool horizontal_angle_calculated = false;
-            Eigen::Matrix<double, 3, 1> scene_p_cpy;
-            double angle_rad = 0.0;
-            // The real vertical resolution has to be calculated using the first and the last
-            // vertical vector and the number of vectors - 1
-            Eigen::Matrix<double, 3, 1> first_vertical_scene_p, last_vertical_scene_p;
+    int height = (*it)->height;
+    int width = (*it)->width;
+
+    double b_x = (width / 2.0) / tan(M_PI/4.0); 
+    double b_y = (height / 2.0) / tan(M_PI/4.0); 
+    
+    // Run through the image horizontally.  
+    for(double horAngle = 0; horAngle < M_PI * 2; horAngle += stepHorizontal) {
+        velodyne_lidar::MultilevelLaserScan::VerticalMultilevelScan vertical_multi_level_scan;
+        curAngle += stepHorizontal;
+        
+        if(curAngle > M_PI/2.0)
+        {
+            counter++;
+            std::cout << "Camera " << counter << std::endl;
+            curAngle -= M_PI/2.0;
+            it++;
+            if(it == mCameras.end())
+                break;
             
-            for(double y = (*it)->lower_pixel; y < (*it)->upper_pixel; y += (*it)->v_steps_pixel) {
-                x_t = (size_t) x;
-                y_t = (size_t) y;
-                
-                distance = 0;
-                
-                if((*it)->image->getScenePoint<double>( x_t, y_t, scene_p )) {
-                    distance = scene_p.norm();
-                    
-                    // TODO: What happens if the first and/or the last point got NaN?
-                    // Answer: Not scaled values are used, enough for calculations.
-                    // Store first point.
-                    
-                    
-                    // The first point of each vertical line has to be used to calculate
-                    // the real horizontal angle of the point.
-                    if(!horizontal_angle_calculated) {
-                        
-                        // Calculate angle in radians between the z-axis and the scene point.
-                        scene_p_cpy = scene_p;
-                        scene_p_cpy.y() = 0;
-                        angle_rad = acos(scene_p_cpy.dot(Eigen::Vector3d::UnitZ()) / scene_p_cpy.norm());
-                        // Assign prefix.
-                        if(scene_p_cpy.x() < 0) {
-                            angle_rad *= -1;
-                        }
-                        // Store in the MultilevelLaserScan structure.
-                        vertical_multi_level_scan.horizontal_angle.rad = angle_rad - (*it)->rotation_y;
-                        horizontal_angle_calculated = true;
-                    }
-                } else {
-                    RTT::log(RTT::Debug) << "NaN-distance have been returned by camera " << 
-                        (*it)->name  << " for pixel (" << x_t << ", " << y_t << ")" << RTT::endlog();
-                }
-                
-                // Store first and last vertical vector.                
-                if(y == (*it)->lower_pixel) {
-                    first_vertical_scene_p = scene_p;
-                }
-                last_vertical_scene_p = scene_p;
-                
-                
-                if(distance < _minimum_distance.get() || distance > _maximum_distance.get()) {
-                    RTT::log(RTT::Debug) << "Distance " << distance << " have exceeded the limit for pixel (" << 
-                        x_t << ", " << y_t << ")" << RTT::endlog();
-                    distance = 0;
-                }
-                
-  
-                velodyne_lidar::MultilevelLaserScan::SingleScan single_scan;
-                single_scan.range = distance * 1000;
-                vertical_multi_level_scan.vertical_scans.push_back(single_scan);
-                counter++;
-            }
-                        
-            // Calculate real vertical start angle.
-            #if 1
-            first_vertical_scene_p.x() = 0;
-            last_vertical_scene_p.x() = 0;
-            vertical_multi_level_scan.vertical_start_angle.rad = 
-                    acos(first_vertical_scene_p.dot(Eigen::Vector3d::UnitZ()) / first_vertical_scene_p.norm());
-            // Assign prefix.
-            if(first_vertical_scene_p.y() > 0) {
-                vertical_multi_level_scan.vertical_start_angle.rad *= -1;
-            }
-            
-            // Calculate real vertical resolution.
-            double angle_between_vertical_vectors = 
-                    acos(first_vertical_scene_p.dot(last_vertical_scene_p) / first_vertical_scene_p.norm());
-            vertical_multi_level_scan.vertical_angular_resolution = angle_between_vertical_vectors / (vertical_multi_level_scan.vertical_scans.size() - 1);
-            
-            #else
-            
-            vertical_multi_level_scan.vertical_start_angle.rad = 
-                    ((*it)->lower_pixel - (*it)->height/2.0) / (*it)->pixel_per_rad_vertical;
-            vertical_multi_level_scan.vertical_angular_resolution = _resolution_vertical.get();
-            
-            #endif
-            
-            RTT::log(RTT::Info) << "Vertical start angle " << vertical_multi_level_scan.vertical_start_angle.rad << 
-                    ", vertical resolution " << vertical_multi_level_scan.vertical_angular_resolution <<
-                    ", angle between upper and lower vectors " << angle_between_vertical_vectors <<
-                    "number of vertical vectors " << vertical_multi_level_scan.vertical_scans.size() << RTT::endlog();
-              
-            multi_level_scan.horizontal_scans.push_back(vertical_multi_level_scan);
+            (*it)->camera_sensor->getDepthImage((*it)->image->data);
         }
-        RTT::log(RTT::Info) << counter << " points have been added by camera " << (*it)->name <<
-                " ID "<< (*it)->sensor_id << RTT::endlog();
+
+//         std::cout << "tan(curAngle "<<curAngle<< "  - M_PI / 4.0) " << tan(curAngle - M_PI / 4.0) << " * b_x " << b_x << " + (width / 2.0) " <<  (width / 2.0)  << std::endl;
+        int x = tan(curAngle - M_PI / 4.0) * b_x + (width / 2.0);
+        
+        for(double verAngle = verticalStartAngle; verAngle < verticalOpeningAngle + verticalStartAngle; verAngle += stepVertical) {
+
+            int y = tan(verAngle) * b_y + (height / 2.0);
+//             std::cout << " X " << x << " Y " << y << std::endl; 
+
+//            distance = (*it)->image->data[y * width + x];
+            std::cout << "distance foo " << distance << std::endl;
+            if((*it)->image->getScenePoint( x, y, scene_p ))
+            {                
+// //                std::cout << "in Image " << scene_p.transpose()<<  std::endl;
+                distance = scene_p.norm();
+            }
+            else
+            {
+                //invalid
+                distance = 0;
+            }
+
+            velodyne_lidar::MultilevelLaserScan::SingleScan single_scan;
+            single_scan.range = distance * 1000;
+            vertical_multi_level_scan.vertical_scans.push_back(single_scan);
+        }            
+        vertical_multi_level_scan.vertical_start_angle = base::Angle::fromRad(verticalStartAngle);
+        vertical_multi_level_scan.vertical_angular_resolution = stepVertical;
+        vertical_multi_level_scan.horizontal_angle = base::Angle::fromRad(-horAngle + M_PI / 4.0);
+
+        multi_level_scan.horizontal_scans.push_back(vertical_multi_level_scan);
     }
+    std::cout << "End " << std::endl;
     _laser_scans.write(multi_level_scan);
 }
